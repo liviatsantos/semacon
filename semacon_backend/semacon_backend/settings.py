018b0b098f82ec5698f97e9c6bff4dc29738ab20
Livia Santos <liviatsantos@gmail.com>
2024-07-10 22:36:12 -0300

    correção nginx e settings.py

--- semacon_backend/semacon_backend/settings.py
+++ semacon_backend/semacon_backend/settings.py
@@ -71,7 +71,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://68.183.63.19:1337'
 ]
 
-CORS_TRUSTED_ORIGINS = [
+CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.0.1:80000',
     'http://127.0.0.0.1:30000',
     'http://68.183.63.19',
